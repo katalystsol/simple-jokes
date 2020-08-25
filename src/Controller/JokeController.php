4698a2806bd626c95dc7aa92004d7c52f44c99da
Don Cranford <dcranford@katalystsolutions.com>
2020-08-24 20:28:52 -0500

    JokeController->store() - implement this method

--- src/Controller/JokeController.php
+++ src/Controller/JokeController.php
@@ -7,7 +7,10 @@ use App\Repository\JokeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class JokeController extends AbstractController
 {
@@ -34,15 +37,39 @@ class JokeController extends AbstractController
     /**
      * Add the joke resource.
      *
+     * @param ValidatorInterface $validator
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     *
      * @return JsonResponse
      * @Route("/api/jokes", name="joke_create", methods={"POST"})
      */
-    public function store(): JsonResponse
+    public function store(ValidatorInterface $validator, Request $request, SerializerInterface $serializer): JsonResponse
     {
-        // TODO implement
+        $date = new \DateTime();
+
         $entityManager = $this->getDoctrine()->getManager();
 
+        $joke = new Joke();
+        $joke->setJoke($request->get('joke'));
+        $joke->setCreatedAt($date);
+        $joke->setUpdatedAt($date);
+
+        $errors = $validator->validate($joke);
+
+        if (count($errors)) {
+            $errorString = (string) $errors;
+            return new JsonResponse(['errors' => $errorString], 422);
+        }
+
         $entityManager->flush();
+
+        $responseData = [
+            'success' => true,
+            'data' => $serializer->serialize($joke, 'json'),
+        ];
+
+        return new JsonResponse($responseData, 201);
     }
 
     /**
